e5322e8c3c0f106cba7921e15da47a40f4f1d71f
20161104598 <1124535694@qq.com>
2018-09-28 17:46:25 +0800

    928.2

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var result: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func number1(_ sender: Any) {
+    }
+    
+    @IBAction func number2(_ sender: Any) {
+    }
+    @IBAction func number3(_ sender: Any) {
+    }
+    @IBAction func number4(_ sender: Any) {
+    }
+    @IBAction func number5(_ sender: Any) {
+    }
+    @IBAction func number6(_ sender: Any) {
+    }
+    @IBAction func number7(_ sender: Any) {
+    }
+    @IBAction func number8(_ sender: Any) {
+    }
+    @IBAction func number9(_ sender: Any) {
+    }
+    @IBAction func number0(_ sender: Any) {
+    }
+    @IBAction func add(_ sender: Any) {
+    }
+    @IBAction func subbtract(_ sender: Any) {
+    }
+    @IBAction func multiply(_ sender: Any) {
+    }
+    @IBAction func divide(_ sender: Any) {
+    }
 }
 
